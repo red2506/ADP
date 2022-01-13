f28ae3536c7ce60e120c5a0c7d13aab29fc08c31
Aneesh <aneesh.cgps@gmail.com>
2022-01-13 14:06:01 +0530

    main program updated

--- attendance.py
+++ attendance.py
@@ -66,6 +66,8 @@ while True:
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
+            print("Name:"+name)
+            print("Access Granted!")
 
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
